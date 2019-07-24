69b0fb688491ae86e94a1211e6436f2b7c11e3f1
goshva <sha.egor@gmail.com>
2019-07-24 16:18:26 +0300

    coupon feedback

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ new Vue({
   el: '#app',
   data: {
     clubs: [],
+    coupons: [],
     email: '',
     reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
     myCouponId:'',  
@@ -13,19 +14,35 @@ new Vue({
     }
   },
   created: function() {
-    this.freeCouponsList()
+    this.clubsList()
   },
   methods: {
+    clubsList: function() {
+      fetch('./content/clubs.json').then((response) => {
+        return response.json().then((json) => {
+        this.clubs = json.clubs
+        this.freeCouponsList()
+        })
+      })
+    },
     freeCouponsList: function() {
       fetch('./content/freeCoupons.php?'+this.coupon).then((response) => {
         return response.json().then((json) => {
-        this.clubs = json
+        this.coupons = json
         })
       })
     },
     couponme: function(coupon) {
       const params  = { row: coupon.CuponOptionRowID, l:coupon.LinkeID, e:coupon.Email  }
-      fetch('./content/couponme.php?', {method: 'POST', body: JSON.stringify(params) }).then((response) => {
+      fetch('./content/couponme.php', {
+          method: 'POST',
+          headers: {
+             'Accept': 'application/json',
+             'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(params)
+      })
+      .then((response) => {
          return response.json().then((json) => {
            this.myCouponId = json
            this.freeCouponsList()
