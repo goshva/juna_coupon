4ade74fff7ccf49bcfc96e4d11d430fcb5f8226f
Egor Sh <sha.egor@gmail.com>
2019-07-24 13:18:11 +0300

    post for email

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ new Vue({
   data: {
     clubs: [],
     email: '',
-    reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/
+    reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
+    myCouponId:'',  
   },
   computed: {
     coupon: function () {
@@ -22,9 +23,9 @@ new Vue({
     couponme: function(coupon) {
       const params  = { row: coupon.CuponOptionRowID, l:coupon.LinkeID, e:coupon.Email  }
       var queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
-      fetch('./content/couponme.php?'+queryString).then((response) => {
+      fetch('./content/couponme.php?'+queryString, {method: 'POST'}).then((response) => {
          return response.json().then((json) => {
-           this.clubs = json
+           this.myCouponId = json
          })
       })
     },
